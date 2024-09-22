b07f7707bb2a06bc5952cf84be752ad455554dca
Alvin Cabe <146591374+Alrwx@users.noreply.github.com>
2024-09-22 16:48:20 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// Calc program chat
+
+const display = document.getElementById("display");
+
+function appendToDisplay(num) {
+    display.value += num;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error...";
+    }
+}
+    
